eb98c8d2f880079d7b3761c96c9bdbb62b9dd7be
cungnguyenb3 <44282437+cungnguyenb3@users.noreply.github.com>
2022-06-09 00:16:14 +0700

    Revert "Update RequestServiceImpl.java"

--- src/main/java/com/example/crawl/service/RequestServiceImpl.java
+++ src/main/java/com/example/crawl/service/RequestServiceImpl.java
@@ -16,39 +16,32 @@ public class RequestServiceImpl implements RequestService {
 	@Override
 	public String getClientIp(HttpServletRequest request) {
 		String ipAddress = request.getHeader("X-Forwarded-For");
-		System.out.println("ip1: " +ipAddress);
-		if(!ipAddress.isEmpty() || ipAddress.equalsIgnoreCase("unknown")) {
-			System.out.println("ip2: " +ipAddress);
+		if(!ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
 			ipAddress = request.getHeader("Proxy-Client-IP");
 		}
-
-		if(!ipAddress.isEmpty() || ipAddress.equalsIgnoreCase("unknown")) {
-			System.out.println("ip3: " +ipAddress);
+		
+		if(!ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
 			ipAddress = request.getHeader("WL-Proxy-Client-IP");
 		}
-
-		if(!ipAddress.isEmpty() || ipAddress.equalsIgnoreCase("unknown")) {
-			System.out.println("ip4: " +ipAddress);
+		
+		if(!ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
 			ipAddress = request.getRemoteAddr();
 			if(LOCALHOST_IPV4.equals(ipAddress) || LOCALHOST_IPV6.equals(ipAddress)) {
 				try {
-					System.out.println("ip1: " +ipAddress);
 					InetAddress inetAddress = InetAddress.getLocalHost();
 					ipAddress = inetAddress.getHostAddress();
 				} catch (UnknownHostException e) {
-					logger.error("Could not get ip from InetAddress" + e.getMessage());
+					e.printStackTrace();
 				}
 			}
 		}
-
-		if(!ipAddress.isEmpty()
+		
+		if(!ipAddress.isEmpty() 
 				&& ipAddress.length() > 15
 				&& ipAddress.indexOf(",") > 0) {
-			System.out.println("ip5: " +ipAddress);
 			ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
 		}
 		
-		System.out.println("ip6: " +ipAddress);
 		return ipAddress;
 	}
 	
