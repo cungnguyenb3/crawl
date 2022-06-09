13f16aaca2038f0cf24031a9799a284c79b21cf9
cung.nguyen <cung.nguyen@formos.com>
2022-06-09 16:25:11 +0700

    fix get ip address

--- src/main/java/com/example/crawl/controller/InfoController.java
+++ src/main/java/com/example/crawl/controller/InfoController.java
@@ -25,33 +25,7 @@ public class InfoController {
 	
 	@RequestMapping("/")
 	public String getClientId(HttpServletRequest request)  {
-
-		try {
-			String ip = requestService.getClientIp(request);
-			
-		    String dbLocation = "your-path-to-mmdb";
-		        
-		    File database = new File(dbLocation);
-		    DatabaseReader dbReader = new DatabaseReader.Builder(database)
-		      .build();
-
-		    InetAddress ipAddress = InetAddress.getByName(ip);
-		    CityResponse response;
-			response = dbReader.city(ipAddress);
-		    String countryName = response.getCountry().getName();
-		    String cityName = response.getCity().getName();
-		    String postal = response.getPostal().getCode();
-		    String state = response.getLeastSpecificSubdivision().getName();
-		    String latitude = 
-		            response.getLocation().getLatitude().toString();
-		    String longitude = 
-		            response.getLocation().getLongitude().toString();
-
-		    GeoIpDTO geoIpDto = new GeoIpDTO(ip, countryName, cityName, postal, state, latitude, longitude);
-		    return geoIpDto.toString();
-
-		} catch (IOException | GeoIp2Exception e) {
-			return e.toString();
-		}
+		String ip = requestService.getClientIp(request);
+		return ip;
 	}
 }
--- src/main/java/com/example/crawl/service/RequestServiceImpl.java
+++ src/main/java/com/example/crawl/service/RequestServiceImpl.java
@@ -1,11 +1,11 @@
 package com.example.crawl.service;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Service;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Service
 public class RequestServiceImpl implements RequestService {
@@ -15,23 +15,34 @@ public class RequestServiceImpl implements RequestService {
 	
 	@Override
 	public String getClientIp(HttpServletRequest request) {
-		String ip = request.getHeader("X-Forwarded-For");  
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-            ip = request.getHeader("Proxy-Client-IP");  
-        }  
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-            ip = request.getHeader("WL-Proxy-Client-IP");  
-        }  
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-            ip = request.getHeader("HTTP_CLIENT_IP");  
-        }  
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
-        }  
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-            ip = request.getRemoteAddr();  
-        }  
-        return ip;  
-	}
-	
+        String ipAddress = request.getHeader("X-Forwarded-For");
+        if(ipAddress == null || ipAddress.isEmpty() || ipAddress.equalsIgnoreCase("unknown")) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+
+        if(ipAddress == null || ipAddress.isEmpty() || ipAddress.equalsIgnoreCase("unknown")) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if(ipAddress == null || ipAddress.isEmpty() || ipAddress.equalsIgnoreCase("unknown")) {
+            ipAddress = request.getRemoteAddr();
+            if(LOCALHOST_IPV4.equals(ipAddress) || LOCALHOST_IPV6.equals(ipAddress)) {
+                try {
+                    InetAddress inetAddress = InetAddress.getLocalHost();
+                    ipAddress = inetAddress.getHostAddress();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if(ipAddress == null || ipAddress.isEmpty()
+                && ipAddress.length() > 15
+                && ipAddress.indexOf(",") > 0) {
+            ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+        }
+
+        return ipAddress;
+    }
+
 }
